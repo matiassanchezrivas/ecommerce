e36713e2d1c3883be218fbe7d2e191fcc2b2f282
ARferraris <ariel.ferraris@hotmail.com.ar>
2018-06-27 19:11:29 -0300

    modelo Order sin relaciones

---
+++ API/db/models/Order.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+const db = require('../db.js')
+const Product = require('./Product')
+
+const 
+Order = db.define('order', {
+  status: {
+      type: Sequelize.ENUM('creado', 'procesando', 'cancelado', 'completado'),
+      allowNull: false
+  },
+  fecha: {
+      type: Sequelize.DATE
+  },
+  total: {
+      type: Sequelize.INTEGER,
+      defaultValue : 0
+  }
+  
+
+})
+
+
+
+
+module.exports = Order;
