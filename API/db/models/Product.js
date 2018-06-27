796ebe7c2720ee1bdf79af5df81c512c43109d6c
Matias <matiassanchezrivas@gmail.com>
2018-06-27 19:11:35 -0300

    Product model terminado

--- API/db/models/Product.js
+++ API/db/models/Product.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
 const db = require('../db.js')
+const Category = require('./Category.js')
+const Review = require('./Review')
 
 const Product = db.define('product', {
   name: {
@@ -12,18 +14,21 @@ const Product = db.define('product', {
   },
   images: {
     type: Sequelize.ARRAY(Sequelize.STRING)
+  },
+  stock: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0
+    }
+  },
+  available: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
   }
-
 })
 
-
+//Product.hasMany(Category, { as: 'Categories' });
+//Product.hasMany(Review, { as: 'Reviews' });
 
 module.exports = Product;
-
-// Nombre
-// Descripción
-// Imagen [ ]
-// Review [ ] FK
-// Stock
-// Disponibilidad
-// Categorías [ ] FK
