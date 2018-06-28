cdd2be104b903cca61583a570d7ed624e04cf04e
Nicolas Bernal <nicolasbernalr@gmail.com>
2018-06-28 12:11:30 -0300

    estructura de rutas y base de user

---
+++ API/routes/user.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = new express.Router();
+
+// modelos que voy a usar 
+// const models = require('../../db/models');
+// const Artist = models.Artist;
+
+module.exports = router;
+
+
+//routers 
