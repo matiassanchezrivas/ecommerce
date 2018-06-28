8db81f02aac03b68988b25eac6c7eef19991da41
Nicolas Bernal <nicolasbernalr@gmail.com>
2018-06-28 15:36:57 -0300

    conectado todo express con las rutas funcionando con test

--- API/app.js
+++ API/app.js
@@ -1,25 +1,26 @@
-const Sequelize = require('sequelize');
 const Chalk = require('chalk')
-
 const express = require('express')
 const db = require('./db/db.js')
 const bodyParser = require('body-parser')
-
 const routes = require('./routes')
-
 const app = express();
 
+
+// express config
 app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(routes)
+app.use('/', routes)
 
 
-app.use((err, req, res, next) => {
-    res.status(500).send(err);
+
+//error handler
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
+  
 
-
-
+// Sync de BD y listen de express
 db.sync()
-    .then(() => app.listen(3001, () => console.log(Chalk.green('Escuchando en el puerto 3001'))));
+    .then(() => app.listen(3002, () => console.log(Chalk.green('Escuchando en el puerto 3002'))));
