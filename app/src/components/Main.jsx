371087af01fb9b67970482f3fee58e0aba641baf
Nicolas Bernal <nicolasbernalr@gmail.com>
2018-07-02 11:46:44 -0300

    navbar en su lugar

--- app/src/components/Main.jsx
+++ app/src/components/Main.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+
 import './Main.css';
 import Button from '@material-ui/core/Button';
+import MenuAppBar from './MenuAppBar'
 
 
 class Main extends Component {
   render() {
     return (
       <div className="App">
-        <Button variant="contained" color="primary">functionando con material ui</Button>
+        <MenuAppBar />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
