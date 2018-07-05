938cde62b6c4dacafb800b9bb400fb48d680aaad
Matias <matiassanchezrivas@gmail.com>
2018-07-05 17:43:46 -0300

    solves axios problem with foreign server, fixes all merges

--- API/app.js
+++ API/app.js
@@ -10,6 +10,29 @@ const cookieSession = require('cookie-session');
 const keys = require('./config/keys.js')
 const passport = require('passport')
 
+const allowCrossOrigin = (req, res, next) => {
+    // intercept OPTIONS method
+    if (req.method === 'OPTIONS') {
+        res.set({
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Accept, Header, Content-Type, access-control-allow-origin',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        });
+        return res.sendStatus(200);
+    }
+    res.set({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+        'Access-Control-Allow-Headers': 'header, Authorization,Content-Type,Accept,Origin,User-Agent,DNT,Cache-Control,X-Mx-ReqToken,Keep-Alive,X-Requested-With,If-Modified-Since',
+        'Access-Control-Allow-Credentials': true,
+    });
+    return next();
+};
+
+app.use(allowCrossOrigin)
+
 //passport cookies
 app.use(cookieSession({
     maxAge: 24 * 60 * 60 * 1000,
