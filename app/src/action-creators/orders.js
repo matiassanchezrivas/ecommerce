c274b81e6002697c95f19f2cadbd36526f4563b5
nicolasbernalr <nicolasbernalr@gmail.com>
2018-07-11 18:45:47 -0300

    redux cart 1era parte

--- app/src/action-creators/orders.js
+++ app/src/action-creators/orders.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { RECEIVE_ORDERS, RECEIVE_ORDER } from '../constants';
+import { RECEIVE_ORDERS, RECEIVE_ORDER, SET_ORDER } from '../constants';
 
 const receiveOrders = (orders) => ({
   type: RECEIVE_ORDERS,
@@ -8,7 +8,12 @@ const receiveOrders = (orders) => ({
 
 const receiveOrder = (order) => ({
   type: RECEIVE_ORDER,
-  album,
+  order,
+});
+
+const setOrder = (order) => ({
+  type: SET_ORDER,
+  order,
 });
 
 export const fetchOrders = () => dispatch =>
@@ -17,6 +22,13 @@ export const fetchOrders = () => dispatch =>
     .then(orders => dispatch(receiveOrders(orders)));
 
 export const fetchOrder = id => dispatch =>
-  axios.get(`/api/albums/${id}`)
+  axios.get(`/api/orders/${id}`)
     .then(res => res.data)
     .then(order => dispatch(receiveOrder(order)));
+
+export const putOrder = (id, contents) => dispatch =>
+  axios.put(`/api/orders/${id}`, contents)
+    .then(res => res.data)
+    .then(order => dispatch(setOrder(order)));
+
+
