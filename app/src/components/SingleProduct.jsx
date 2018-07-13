e823ae5087f912f262c3089b64c6c336883ef9e2
Juan <lazo.juan@hotmail.com>
2018-07-13 12:36:02 -0300

    connect singleProduct

--- app/src/components/SingleProduct.jsx
+++ app/src/components/SingleProduct.jsx
@@ -13,40 +13,90 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import ShoppingCart from '@material-ui/icons/ShoppingCart'
 import { connect } from 'react-redux';
 axios.defaults.baseURL='http://localhost:3002'
  
 const db =[
-    {Nombre:"iphone 5", descripcion:'Lizards are a widespread group of squamate reptiles, with over 6,000 species, rangingacross all continents except Antarctica',imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWfnD5ehNmZO3CeClig1Zacs8hybmYyRevuF6ajW7Utd2ToQh2",categoria: "categoria 1"}
-  ]
+    {id:1, img:'https://images.evo.com/imgp/700/122201/538568/dc-focus-snowboard-2018-145.jpg', title:'DC Focus Snowboard 2017', price:150, cantidad:2,categoria: 'categoria 1'},
+
+    ]
 
 
 
 
 const styles = theme => ({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      padding: theme.spacing.unit * 2,
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
+    root:{
+        padding: '2rem',
+        display: 'flex',
+        flexDirection: 'row',
+        flex: 1, 
+        minHeight: '100%', 
     },
     itemsList:{
         flex: 0.7,
         paddingRight: '1rem',
     },
     buyBox:{
-        background: 'white',
+        borderRadius: '5px 5px 0 0',
         flex: 0.3,
     },
+    buyBoxBody:{
+        display: 'grid',
+        background: 'white',
+    },
+    buyBoxHeader:{
+        minHeight: '3rem',
+        color: 'white' ,
+        background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+    },
+    buyBoxButton1: {
+        background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+        fontSize: '0.81rem',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 15,
+        margin: '0.5rem',
+        marginBottom: '3rem',
+        padding: '0 20px',
+        boxShadow: '0 3px 5px 2px rgba(33, 203, 243, .3)',
+    },
+    buyBoxButton2: {
+        fontSize: '0.81rem',
+        background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 15,
+        margin: '0.5rem',
+        marginBottom: '3rem',
+        padding: '0 20px',
+        boxShadow: '0 3px 5px 2px rgba(33, 203, 243, .3)',
+    },
+    punch:{
+        display: 'inline-block',
+        padding: '1rem',
+        margin: 'auto',
+        fontSize: 'small',
+        fontWeight: 100,
+    },
+    icon:{
+        color: '98d1fe',
+        position: 'relative',
+        top: '0.5rem',
+    },
     total:{
-        color: 'grey' ,
+        margin: '3rem 3rem 2rem 3rem',
+        borderTop: '1px solid #DDD',
+        borderBottom: '1px solid #DDD',
+        color: '#727272' ,
         flex: 0.8,
     },
     checkout:{
         flex: 0.2,
     },
+    
     button: {
         fontSize: '0.81rem',
         background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
@@ -54,9 +104,9 @@ const styles = theme => ({
         border: 0,
         color: 'white',
         height: 15,
+        margin: '0.5rem',
         padding: '0 20px',
         boxShadow: '0 3px 5px 2px rgba(33, 203, 243, .3)',
-        margin:20
     },
     textField: {
         marginLeft: theme.spacing.unit,
@@ -72,20 +122,17 @@ const styles = theme => ({
         flex: 1,
     },
     imagen: {
-        height: '20rem',       
+        height: '9rem',
+        width: '10rem',
         flex: 0.3,
-        margin:40,
-        textAlign: 'center',        
     },
     ImgProd: {
-        maxHeight: '55rem',
-        margin:40,
-        
+        maxHeight: '8rem',
     },
     title:{
         paddingRight: '0.9rem',
         flex: 0.5,
-        textAlign: 'center',
+        textAlign: 'left',
     },
     price:{
         color: 'grey',
@@ -93,12 +140,11 @@ const styles = theme => ({
     },
     cantidad:{
         flex: 0.1,
-    },
-    Appbar: {
-      background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
-    }  
-   
-  });
+    }
+
+
+  
+});
 
 
 
@@ -129,44 +175,56 @@ class SingleProduct extends React.Component{
       }
   
     render(){
-        const {classes}=this.props  
-        
-        return(    <div>
+        const { classes, items, total, eliminar, cantidad, vaciar, userC } =this.props;
+  return (
+    <div className={classes.root}>
+        <div className={classes.itemsList}>
+          <div>
+                <div key={this.state.currentProduct.id} className={classes.product}>
+    
                     <div className={classes.imagen}>
-                    <div>
-                    
+                        <img src={this.state.currentProduct.images} alt={this.state.currentProduct.title} className={classes.ImgProd}/>
+                    </div>
+
                     <div className={classes.title}>
-                        <h4>{this.state.currentProduct.name}</h4>
-                        <Button className={classes.button}onClick={()=>this.props.addProductCart(this.state.currentProduct)}>
-                        comprar
+                        <h2>{this.state.currentProduct.name}</h2>
+                        <h4>{this.state.currentProduct.description}</h4>
+                        <Button onClick={()=>this.props.addProductCart(this.state.currentProduct)} className={classes.button}>
+                        Comprar
                         </Button>
-                        <Button className={classes.button} >
-                        detalle
-                        </Button>
-                    </div> 
+                    </div>
+            
                     <div className={classes.price}>
-                        <h4>${200}</h4>
+                        <h4>${this.state.currentProduct.price}</h4>
                     </div>
                 
                     <div className={classes.cantidad}>
-                            <TextField
-                            label="Cantidad"
-                            type="number"
-                            className={classes.textField}
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                            margin="normal"
-                            />
-                    </div>   
-                    </div>                
-                        
-                    </div>
+                           
+                    </div>                    
 
-                   
+                </div>
+        </div>
             
-                   
-    </div>)
+            
+        </div>
+        <div className={classes.buyBox}>
+
+            <div className={classes.buyBoxHeader}>
+                <p className={classes.punch}>{'Review'}</p>
+            </div>
+            <div className={classes.buyBoxBody}>
+                <div className={classes.total}>
+                    <h3>{userC}</h3>
+                    <h3>{}</h3>
+                </div>
+                <div className={classes.checkout}>
+                  
+                </div>
+            </div>
+
+        </div>
+    </div>
+  );
     
 };    
 }
--- app/src/reducers/product-reducer.js
+++ app/src/reducers/product-reducer.js
@@ -1,14 +1,10 @@
 import { RECEIVE_PRODUCTS, SET_CURRENT_PRODUCT } from '../constants';
 
 const initialState = {
-<<<<<<< HEAD
-  cart: [],
-=======
   product: {
 
   },
   products: []
->>>>>>> 7951e91b098435b9501fe5c2120ca043d955dd61
 
 };
 
